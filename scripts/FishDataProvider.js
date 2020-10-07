b69691a75496d63e7f965d08f2f991e2c1d8d3b5
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-10-07 15:56:30 -0500

    Added FishDataProvider, main.js gets list off ish and iterates over each one

---
+++ scripts/FishDataProvider.js
@@ -0,0 +1,40 @@
+
+
+const fishCollection = [
+    {
+        species: "Boeseman's rainbowfish (Melanotaenia boesemani)",
+        name: "Bowie",
+        image: "https://upload.wikimedia.org/wikipedia/commons/1/1b/Sunny1.jpg",
+        length: 5.1,
+        location: "",
+        diet: "Algae, small crustaceans, and aquatic insects",
+    },
+    {
+        species: "Empire gudgeon (Hypseleotris compressa)",
+        name: "King Kong",
+        image: "https://upload.wikimedia.org/wikipedia/commons/7/72/Hypseleotris_compressa.jpg",
+        length: 4.7,
+        location: "Papa New Guinea",
+        diet: "Small crustaceans, insects, algae, and aquatic plants",
+    },
+    {
+        species: "Dragon goby/Violet goby (Gobioides broussonnetii)",
+        name: "Scoot",
+        image: "https://upload.wikimedia.org/wikipedia/commons/c/c9/Gobioides_broussonnetii_-_Gobie_violet_-_Aqua_Porte_Dor%C3%A9e_13.JPG",
+        length: 3.9,
+        location: "Shell Beach (Georgetown, Guyana)",
+        diet: "Edible material sifted from substrate",
+    },
+    {
+        species: "Pumpkinseed (Lepomis gibbosus)",
+        name: "Pumpkin",
+        image: "https://upload.wikimedia.org/wikipedia/commons/e/eb/Lepomis_gibbosus_PAQ.jpg",
+        length: 11,
+        location: "Lake St. Clair (Ontario, Canada)",
+        diet: "Insects, mosquito larvae, small molluscs and crustaceans, worms, minnow fry, and other smaller pumpkinseeds",
+    }
+]
+
+export const useFish = () => {
+    return fishCollection.slice()
+}
